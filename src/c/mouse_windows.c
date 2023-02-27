54175b0ea6a368bcff693afa0456c7373971eda8
Adam Oates <adam.oates@criptext.com>
2023-02-27 13:24:59 -0600

    add: functions for Windows compatibility

--- src/c/mouse_windows.c
+++ src/c/mouse_windows.c
@@ -2,15 +2,22 @@
 #include "mouse_windows.h"
 
 struct Position get_mouse_pos() {
-	struct Position pos = {-1, -1};
-
-
-
+	POINT p;
+	GetCursorPos(&p);
+	struct Position pos = {p.x, p.y};
 	return pos;
 }
 
 void set_mouse_pos(int x, int y) {
-	// int nX = x * 65535 / GetSystemMetrics(SM_CXSCREEN);
-	// int nY = y * 65535 / GetSystemMetrics(SM_CYSCREEN);
-	// mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, nX, nY, 0, 0);
+	int nX = x * 65535 / GetSystemMetrics(SM_CXSCREEN);
+	int nY = y * 65535 / GetSystemMetrics(SM_CYSCREEN);
+	mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, nX, nY, 0, 0);
+}
+
+struct Size screen_size() {
+	struct Size size = {
+		GetSystemMetrics(SM_CXSCREEN),
+		GetSystemMetrics(SM_CYSCREEN)
+	};
+	return size;
 }
