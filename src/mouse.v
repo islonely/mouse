4d2a5e49341452ab91e61b7432c01756357640b6
Adam Oates <adam.oates@criptext.com>
2024-05-19 14:02:12 -0500

    vfmt

--- src/mouse.v
+++ src/mouse.v
@@ -11,7 +11,6 @@ $if linux {
 } $else $if windows {
 	#include "mouse_windows.h"
 } $else $if macos {
-
 }
 
 // See .c files for comments.
@@ -20,26 +19,28 @@ __global:
 	x int
 	y int
 }
+
 pub type Position = C.Position
 
 struct C.Size {
 __global:
-	width int
+	width  int
 	height int
 }
+
 pub type Size = C.Size
 
 fn C.get_mouse_pos() C.Position
 fn C.set_mouse_pos(int, int)
 fn C.screen_size() C.Size
 
-[inline]
+@[inline]
 pub fn get_pos() (int, int) {
 	pos := C.get_mouse_pos()
 	return pos.x, pos.y
 }
 
-[inline]
+@[inline]
 pub fn get_pos_opt() ?(int, int) {
 	x, y := get_pos()
 	if -1 in [x, y] {
@@ -48,12 +49,12 @@ pub fn get_pos_opt() ?(int, int) {
 	return x, y
 }
 
-[inline]
+@[inline]
 pub fn set_pos(x int, y int) {
 	C.set_mouse_pos(x, y)
 }
 
-[inline]
+@[inline]
 pub fn screen_size() Size {
 	return C.screen_size()
 }
